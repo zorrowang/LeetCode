ff2bfa291988c8690c9846c0fca06810fef7230c
Hongwei Wang <Zorro@hongwei-wangs-macbook-pro-2.local>
2013-06-02 20:13:41 -0400

    Adding solution of Longest Common Prefix

---
+++ Longest_Common_Prefix.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        if (strs==null || strs.length==0) return "";
+        int i=0;
+        boolean flag = false;
+        while(true){
+            for (int j=0; j<strs.length; j++){
+                if (i>= strs[j].length() || strs[0].charAt(i) != strs[j].charAt(i)){
+                    flag = true;
+                    break;
+                }
+            }
+            if (flag)   break;
+            i++;
+        }
+        
+        if (i==0)
+            return "";
+        else
+            return strs[0].substring(0, i);
+    }
+}
+
