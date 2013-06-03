73ced6a5d74561f9ddb0ca81dd5d93986c906414
ZorroWang <zorrowhw@gmail.com>
2013-06-02 21:22:25 -0400

    Adding Valid Parentheses

---
+++ Valid_Parentheses.java
@@ -0,0 +1,30 @@
+public class Solution {
+    public boolean isValid(String s) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        LinkedList<Character> stack = new LinkedList<Character>();
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(isLeftParenthese(ch))    stack.push(ch);
+            else{
+                if(!stack.isEmpty() && isPair(stack.peek(), ch))    stack.pop();
+                else    return false;
+            }
+        }
+        
+        return stack.isEmpty();
+    }
+    
+    private boolean isPair(char p, char q){
+        if (p=='(' && q==')')   return true;
+        if (p=='[' && q==']')   return true;
+        if (p=='{' && q=='}')   return true;
+        return false;
+    }
+    
+    private boolean isLeftParenthese(char p){
+        if (p=='(' || p=='[' || p=='{') return true;
+        return false;
+    }
+}
+
