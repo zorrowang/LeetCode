7d4bbd128d89e183f2123c9356cf53af0f893c66
ZorroWang <zorrowhw@gmail.com>
2013-06-06 16:01:47 -0400

    Adding Balanced Binary Tree

---
+++ Balanced_Binary_Tree.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isBalanced(TreeNode root) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        if (root==null) return true;
+        int left = depth(root.left);
+        int right = depth(root.right);
+        return Math.abs(left-right)<=1 && isBalanced(root.left) && isBalanced(root.right);
+    }
+    
+    public int depth(TreeNode root){
+        if (root==null) return 0;
+        return Math.max(depth(root.left), depth(root.right))+1;
+    }
+}
