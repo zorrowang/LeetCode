561a0ea250cd4e392863e1d4852723b10e3575ab
ZorroWang <zorrowhw@gmail.com>
2013-06-10 18:19:26 -0400

    Adding solution of Remove Element

---
+++ Remove_Element.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public int removeElement(int[] A, int elem) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        int cur=0; 
+        for (int i=0; i<A.length; i++){
+            if (A[i]==elem) continue;
+            else{
+                A[cur++] = A[i];
+            }
+        }
+        return cur;
+    }
+}
