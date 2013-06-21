56fbfd1ebe88199d8e6ec751c9d642f70d9eb98b
ZorroWang <zorrowhw@gmail.com>
2013-06-21 10:40:48 -0400

    Adding count and say

---
+++ Count_and_Say.java
@@ -0,0 +1,40 @@
+public class Solution {
+    public String countAndSay(int n) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        if (n<=0)   return "";
+        String ret = "1";
+        for (int i=2; i<=n; i++){
+            ret = transfer(ret);
+        }
+        
+        return ret;
+    }
+    
+    public String transfer(String str){
+        LinkedList<Integer> nums = new LinkedList<Integer>();
+        LinkedList<Integer> counts = new LinkedList<Integer>();
+        
+        int num = str.charAt(0)-'0';
+        int count = 1;
+        for (int i=1; i<str.length(); i++){
+            if (num == str.charAt(i)-'0')
+                count++;
+            else{
+                nums.add(num);
+                counts.add(count);
+                num = str.charAt(i)-'0';
+                count = 1;
+            }
+        }
+        nums.add(num);
+        counts.add(count);
+        
+        StringBuilder ret = new StringBuilder();
+        for(int i=0; i<nums.size(); i++){
+            ret.append(counts.get(i));
+            ret.append(nums.get(i));
+        }
+        return ret.toString();
+    }
+}
