a880c0699d7f4a98d43262a0fca2bd4de78cdc23
ZorroWang <zorrowhw@gmail.com>
2013-07-08 15:19:47 -0400

    Adding rotate image

---
+++ Rotate_Image.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public void rotate(int[][] matrix) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        if (matrix==null || matrix.length<2)    return;
+        for (int i=0; i<matrix.length/2; i++)
+            rotate(matrix, i);
+    }
+    
+    public void rotate(int[][] matrix, int level) {
+        int t1, t2;
+        for (int i=level; i<matrix.length-level-1; i++){
+            t1 = matrix[i][matrix.length-level-1];
+            matrix[i][matrix.length-level-1] = matrix[level][i];
+            t2 = matrix[matrix.length-level-1][matrix.length-i-1];
+            matrix[matrix.length-level-1][matrix.length-i-1] = t1;
+            t1 = matrix[matrix.length-i-1][level];
+            matrix[matrix.length-i-1][level] = t2;
+            matrix[level][i] = t1;
+        }
+    }
+}
