02a26752d631ac6042906151a064739fc000b2c8
ZorroWang <zorrowhw@gmail.com>
2013-07-09 17:48:28 -0400

    Adding solution #2 of search in rotated sorted array

--- Search_in_Rotated_Sorted_Array.java
+++ Search_in_Rotated_Sorted_Array.java
@@ -35,3 +35,30 @@ public class Solution {
     }
 }
 
+// Solution #2
+public class Solution {
+    public int search(int[] A, int target) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        if (A==null)    return -1;
+        return searchHelper(A, target, 0, A.length-1);
+    }
+    
+    public int searchHelper(int[] A, int target, int start, int end){
+        if (start>end)  return -1;
+        int mid = (start+end)/2;
+        if (A[mid]==target) return mid;
+        else if (A[mid]>target){
+            if (A[start]>target && A[mid]>A[end])
+                return searchHelper(A, target, mid+1, end);
+            else
+                return searchHelper(A, target, start, mid-1);                
+        }
+        else{
+            if (A[end]<target && A[start]>A[mid])
+                return searchHelper(A, target, start, mid-1); 
+            else
+                return searchHelper(A, target, mid+1, end);
+        }
+    }
+}
