0a36bd21e757297c8f71daacd7aab1603c948e2a
ZorroWang <zorrowhw@gmail.com>
2013-07-09 18:00:21 -0400

    Adding search in rotated sorted array II

--- Search_in_Rotated_Sorted_Array_II.java
+++ Search_in_Rotated_Sorted_Array_II.java
@@ -2,59 +2,31 @@ public class Solution {
     public boolean search(int[] A, int target) {
         // Start typing your Java solution below
         // DO NOT write main() function
-        if (A==null || A.length==0) return false; 
-        int start = searchHelper(A, 0, A.length-1);
-        boolean ret1 = searchHelper(A, target, 0, start-1);
-        boolean ret2 = searchHelper(A, target, start, A.length-1);
-        return ret1||ret2;
+        if (A==null)    return false;
+        return searchHelper(A, target, 0, A.length-1);
     }
     
     public boolean searchHelper(int[] A, int target, int start, int end){
         if (start>end)  return false;
         int mid = (start+end)/2;
-        if (A[mid]>target)
-            return searchHelper(A, target, start, mid-1);
-        else if (A[mid]<target)
-            return searchHelper(A, target, mid+1, end);
-        else
-            return true;
-    }
-    
-    public int searchHelper(int[] A, int start, int end){
-        if (start>end) return -1;
-        if (start==end) return start;
-        int mid = (start+end)/2;
-        if (A[mid]>A[start] && A[mid]>A[end])
-            return searchHelper(A, mid+1, end);
-        else if (A[mid]<A[start] && A[mid]<A[end]){
-            if (mid==0 || (mid>0&&A[mid-1]>A[mid]))
-                return mid;
-            return searchHelper(A, start, mid-1);
+        if (A[mid]==target) return true;
+        else if (A[mid]>target){
+            if (A[start]==A[end])
+                return searchHelper(A, target, start+1, end);
+                
+            if (A[start]>target && A[mid]>A[end])
+                return searchHelper(A, target, mid+1, end);
+            else
+                return searchHelper(A, target, start, mid-1);                
         }
         else{
-            int temp = A[start]<A[end]?start:end;
-            if (A[mid] == A[temp]){
-                if (A[start]==A[end]){
-                    int in1 = searchHelper(A, start, mid-1);
-                    int in2 = searchHelper(A, mid+1, end);
-                    return in1==-1?in2:(in2==-1?in1:(A[in1]<A[in2]?in1:in2));
-                }
+            if (A[start]==A[end])
+                return searchHelper(A, target, start+1, end);
                 
-                if (mid==0 || (mid>0&&mid<temp&&A[mid-1]>A[mid]))
-                    return mid;
-                else if (mid==A.length-1 || (mid<A.length-1&&mid>temp&&A[mid+1]>A[mid]))
-                    return mid;
-                else{
-                    if (A[start]<A[end])
-                        return searchHelper(A, mid+1, end);
-                    else
-                        return searchHelper(A, start, mid-1);
-                }
-            }
+            if (A[end]<target && A[start]>A[mid])
+                return searchHelper(A, target, start, mid-1); 
             else
-                return A[start]<A[end]?start:end;
+                return searchHelper(A, target, mid+1, end);
         }
     }
 }
-
-
