d92a3fb0350c3f097e4e29b1ff6645072038e6fa
ZorroWang <zorrowhw@gmail.com>
2013-07-11 23:09:30 -0400

    Adding parlindrome partitioning II

--- Palindrome_Partitioning_II.java
+++ Palindrome_Partitioning_II.java
@@ -3,31 +3,22 @@ public class Solution {
         // Start typing your Java solution below
         // DO NOT write main() function
         if(s==null || s.length()<=1)    return 0;
+        boolean[][] palindrome = new boolean[s.length()][s.length()];
+        int[] ret = new int[s.length()+1];
         
-        int[] dp = new int[s.length()+1];
-        Arrays.fill(dp, Integer.MAX_VALUE);
-        dp[0] = 0;
+        for(int i=0; i<s.length(); i++)
+            ret[i] = s.length()-i;
         
-        for (int i=1; i<=s.length(); i++){
-            for (int j=i-1; j>=0; j--){
-                if (isPalindrome(s.substring(j, i))){
-                    if (j==0)   
-                        dp[i] = 0;
-                    else
-                        dp[i] = Math.min(dp[j]+1, dp[i]);
+        for (int i=s.length()-1; i>=0; i--){
+            for (int j=i; j<s.length(); j++){
+                if (s.charAt(i)==s.charAt(j)&&(j-i<=1||palindrome[i+1][j-1])){
+                    palindrome[i][j]=true;
+                    ret[i] = Math.min(ret[i], ret[j+1]+1);
                 }
             }
         }
         
-        return dp[s.length()];
-    }
-    
-    public boolean isPalindrome(String s){
-        int i=0, j=s.length()-1;
-        while(i<j){
-            if (s.charAt(i++)!=s.charAt(j--))
-                return false;
-        }
-        return true;
+        return ret[0]-1;
     }
 }
+
