c881ef31cd6a15b1dd2cefb797464929c103c819
ZorroWang <zorrowhw@gmail.com>
2013-08-20 13:59:13 -0400

    Updating longest consecutive sequence

--- Longest_Consecutive_Sequqence.java
+++ Longest_Consecutive_Sequqence.java
@@ -3,6 +3,7 @@ public class Solution {
         // Start typing your Java solution below
         // DO NOT write main() function
         HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        int ret = 0;
         for(int i=0; i<num.length; i++){
             if (!map.containsKey(num[i])){
                 int left=0, right=0;
@@ -13,11 +14,10 @@ public class Solution {
                 map.put(num[i], left+right+1);
                 if (left!=0)  map.put(num[i]-map.get(num[i]-1), left+right+1);
                 if (right!=0) map.put(num[i]+map.get(num[i]+1), left+right+1);
+                ret = Math.max(ret, left+right+1);
             }
         }
-        int ret = 0;
-        for(int i : map.values())
-            ret = Math.max(i, ret);
+        
         return ret;
     }
 }
