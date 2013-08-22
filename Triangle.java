73b7c7378f7512da8928df9bd48421ae26f764c6
ZorroWang <zorrowhw@gmail.com>
2013-08-22 17:54:29 -0400

    Adding top-down solution for Triangle

--- Triangle.java
+++ Triangle.java
@@ -17,3 +17,27 @@ public class Solution {
     }
 }
 
+// Top-down solution using O(n) space
+public class Solution {
+    public int minimumTotal(ArrayList<ArrayList<Integer>> triangle) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        int[] ret = new int[triangle.size()];
+        for(int i=0; i<triangle.size(); i++){
+            ArrayList<Integer> list = triangle.get(i);
+            for(int j=list.size()-1; j>=0; j--){
+                if (j==0)
+                    ret[j] = ret[j]+list.get(j);
+                else if (j==list.size()-1)
+                    ret[j] = ret[j-1]+list.get(j);
+                else 
+                    ret[j] = Math.min(ret[j-1], ret[j])+list.get(j);
+            }
+        }
+        
+        int min = ret[0];
+        for (int i=1; i<ret.length; i++)
+            min = Math.min(min, ret[i]);
+        return min;
+    }
+}
