2e8d0c0dccd8363a2fecea5a81b92eb32a18efb0
ZorroWang <zorrowhw@gmail.com>
2013-08-26 15:31:22 -0400

    Adding O(n) solution for balanced binary tree

--- Balanced_Binary_Tree.java
+++ Balanced_Binary_Tree.java
@@ -22,3 +22,22 @@ public class Solution {
         return Math.max(depth(root.left), depth(root.right))+1;
     }
 }
+
+// Improve performance to O(n)
+public class Solution {
+    public boolean isBalanced(TreeNode root) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        int ret = getHeight(root);
+        return ret!=-1;
+    }
+    
+    public int getHeight(TreeNode root) {
+        if (root==null) return 0;
+        int left = getHeight(root.left);
+        int right = getHeight(root.right);
+        if (left==-1||right==-1||Math.abs(left-right)>1)
+            return -1;
+        return Math.max(left, right)+1;
+    }
+}
