1991e10c54261db89f8635f51080c21e71ee79a6
ZorroWang <zorrowhw@gmail.com>
2015-01-18 20:18:04 -0500

    update word break

--- Word_Break.java
+++ Word_Break.java
@@ -16,3 +16,23 @@ public class Solution {
         return false;
     }
 }
+
+//DP solution
+public class Solution {
+    public boolean wordBreak(String s, HashSet<String> dict) {
+        if (s==null || s.length()==0)
+            return false;
+        boolean[] ret = new boolean[s.length()+1];
+        ret[0] = true;
+        
+        for (int i=1; i<=s.length(); i++){
+            for (int j=0; j<i; j++){
+                if (ret[j] && dict.contains(s.substring(j,i))){
+                    ret[i] = true;
+                    break;
+                }
+            }
+        }
+        return ret[s.length()];
+    }
+}
