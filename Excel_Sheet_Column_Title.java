d6d08d00e41f7f34c1645ea9c0414ab2b2758847
ZorroWang <zorrowhw@gmail.com>
2015-02-04 23:24:35 -0500

    Add excel sheet column title

--- Excel_Sheet_Column_Title.java
+++ Excel_Sheet_Column_Title.java
@@ -1,14 +1,12 @@
 public class Solution {
-    public double pow(double x, int n) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        if (n==0)   return 1;
-        if (n==1)   return x;
-        boolean neg = (n<0)?true:false;
-        n = neg?-n:n;
-        double ret = pow(x, n/2);
-        if (n%2==1)  ret = ret*ret*x;
-        else    ret = ret*ret;
-        return neg?1/ret:ret;
+    public String convertToTitle(int n) {
+        String ret = "";
+        while(n > 0){
+            int num = (n-1)%26;
+            char ch = (char)('A'+num);
+            ret = ch+ret;
+            n = (n-1)/26;
+        }
+        return ret;
     }
 }
