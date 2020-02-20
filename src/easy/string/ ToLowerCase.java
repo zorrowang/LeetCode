dfa775260fb329e2b5aada14060f73e64cc0dbeb
Hongwei Wang <zorrowanghw@gmail.com>
2020-02-20 15:29:53 -0500

    Add 709 and 905 - easy

---
+++ src/easy/string/ ToLowerCase.java
@@ -0,0 +1,12 @@
+package src.easy.string;
+
+class ToLowerCase {
+    public String toLowerCase(String str) {
+        char[] charArray = str.toCharArray();
+        for (int i=0; i<charArray.length; i++) {
+            if (Character.isLetter(charArray[i]) && charArray[i]>='A' && charArray[i]<='Z')
+                charArray[i] = (char)(charArray[i]-'A'+'a');
+        }
+        return String.valueOf(charArray);
+    }
+}
