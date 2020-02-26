653c00c5c612da1470850608364d64d3f48d323d
Hongwei Wang <zorrowanghw@gmail.com>
2020-02-26 15:29:38 -0500

    Move 83 - easy

--- src/easy/linkedlist/RemoveDuplicatesSortedList.java
+++ src/easy/linkedlist/RemoveDuplicatesSortedList.java
@@ -1,15 +1,10 @@
+package src.easy.linkedlist;
+
 /**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
- * }
+ * Definition for singly-linked list. public class ListNode { int val; ListNode
+ * next; ListNode(int x) { val = x; next = null; } }
  */
-public class Solution {
+public class RemoveDuplicatesSortedList {
     public ListNode deleteDuplicates(ListNode head) {
         // Start typing your Java solution below
         // DO NOT write main() function
