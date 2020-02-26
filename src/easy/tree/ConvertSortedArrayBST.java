794577fcd1fbc515ab7da38e335c8fd70a73c1ba
Hongwei Wang <zorrowanghw@gmail.com>
2020-02-26 15:58:31 -0500

    Move 108 - easy

--- src/easy/tree/ConvertSortedArrayBST.java
+++ src/easy/tree/ConvertSortedArrayBST.java
@@ -1,13 +1,10 @@
+package src.easy.tree;
+
 /**
- * Definition for binary tree
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
+ * Definition for binary tree public class TreeNode { int val; TreeNode left;
+ * TreeNode right; TreeNode(int x) { val = x; } }
  */
-public class Solution {
+public class ConvertSortedArrayBST {
     public TreeNode sortedArrayToBST(int[] num) {
         // Start typing your Java solution below
         // DO NOT write main() function
