f3452784f7f43301d512e3f4ed5dbefc294fe71f
Hongwwei Wang <zorrowanghw@gmail.com>
2020-03-01 17:06:34 -0500

    Add 771 - easy

---
+++ src/easy/string/JewelsStones.java
@@ -0,0 +1,18 @@
+package src.easy.string;
+
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+  public int numJewelsInStones(String J, String S) {
+      if (J==null || S==null || J.isEmpty() || S.isEmpty()) return 0;
+      Set<Character> m = new HashSet<>();
+      for (int i=0; i<J.length(); i++)
+          m.add(J.charAt(i));
+      int ret = 0;
+      for (int i=0; i<S.length(); i++)
+          if (m.contains(S.charAt(i)))
+              ret++;
+      return ret;
+  }
+}
