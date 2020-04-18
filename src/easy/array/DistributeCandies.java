cdb6bf9d900cf36e486e82863abb6da8801a27b9
Hongwwei Wang <zorrowanghw@gmail.com>
2020-04-18 19:35:21 -0400

    Add 575 - easy

--- src/easy/array/DistributeCandies.java
+++ src/easy/array/DistributeCandies.java
@@ -4,19 +4,19 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class DistributeCandies {
-  public int distributeCandies(int[] candies) {
-    Set<Integer> set = new HashSet<>();
-    int count=0;
-    
-    for(int i=0; i<candies.length; i++) {
-        if (!set.contains(candies[i])) {
-            set.add(candies[i]);
-            count++;
-            if (count == candies.length/2)
-                break;
+    public int distributeCandies(int[] candies) {
+        Set<Integer> set = new HashSet<>();
+        int count=0;
+        
+        for(int i=0; i<candies.length; i++) {
+            if (!set.contains(candies[i])) {
+                set.add(candies[i]);
+                count++;
+                if (count == candies.length/2)
+                    break;
+            }
         }
-    }
 
-    return count;
-}
+        return count;
+    }
 }
