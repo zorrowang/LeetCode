25f68df4b7286ee04e04609dd95d68c0073a3ceb
Hongwwei Wang <zorrowanghw@gmail.com>
2020-05-07 12:29:59 -0400

    Update 290 Word Pattern

--- src/java/easy/string/WordPattern.java
+++ src/java/easy/string/WordPattern.java
@@ -1,8 +1,13 @@
-public class Solution {
+package src.java.easy.string;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class WordPattern {
     public boolean wordPattern(String pattern, String str) {
         String[] array = str.split(" ");
         if (pattern.length() != array.length)  return false;
-        HashMap<Character, String> map = new HashMap<>();
+        Map<Character, String> map = new HashMap<>();
         for (int i=0; i<pattern.length(); i++) {
             char c = pattern.charAt(i);
             if (map.containsKey(c)) {
