aa4166f42316c69500eefbc6f788563280c39c04
Hongwwei Wang <zorrowanghw@gmail.com>
2020-05-26 18:59:58 -0400

    Update 329 Longest Increasing Path in a Matrix

--- src/java/hard/backtracking/LongestIncreasingPathMatrix.java
+++ src/java/hard/backtracking/LongestIncreasingPathMatrix.java
@@ -1,33 +1,34 @@
 package src.java.hard.backtracking;
 
 public class LongestIncreasingPathMatrix {
-    // Backtracking (DFS) solution - TLE
-    int max = 0;
     public int longestIncreasingPath(int[][] matrix) {
+        int max = 0;
         if (matrix==null || matrix.length==0)   return max;
-        boolean[][] visited = new boolean[matrix.length][matrix[0].length];
+        int[][] cache = new int[matrix.length][matrix[0].length];
         for (int i=0; i<matrix.length; i++) {
             for (int j=0; j<matrix[i].length; j++) {
-                visited[i][j] = true;
-                longestIncreasingPath(matrix, i, j, visited, 1);
-                visited[i][j] = false;
+                int t = dfs(matrix, i, j, cache);
+                max = Math.max(max, t);
             }
         }
         return max;
     }
     
-    public void longestIncreasingPath(int[][] matrix, int startM, int startN, boolean[][] visited, int cur) {
-        max = Math.max(max, cur);
+    public int dfs(int[][] matrix, int startM, int startN, int[][] cache) {
+        if (cache[startM][startN]>0)    
+            return cache[startM][startN];
+        int max = 1;
         int[] directM = {1, -1, 0, 0};
         int[] directN = {0, 0, 1, -1};
         for (int i=0; i<4; i++) {
             int newM = startM + directM[i];
             int newN = startN + directN[i];
-            if (newM >=0 && newM < matrix.length && newN >= 0 && newN < matrix[0].length && !visited[newM][newN] && matrix[startM][startN] < matrix[newM][newN]) {
-                visited[newM][newN] = true;
-                longestIncreasingPath(matrix, newM, newN, visited, cur+1);
-                visited[newM][newN] = false;
+            if (newM >=0 && newM < matrix.length && newN >= 0 && newN < matrix[0].length && matrix[startM][startN] < matrix[newM][newN]) {
+                int t = dfs(matrix, newM, newN, cache);
+                max = Math.max(max, t+1);
             }
         }
+        cache[startM][startN] = max;
+        return max;
     }
 }
