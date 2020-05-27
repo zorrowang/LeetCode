6de8220be4483ce39e6fbb6177e546cc127e8e12
Hongwwei Wang <zorrowanghw@gmail.com>
2020-05-26 22:32:27 -0400

    Add 397 Integer Replacement

---
+++ src/java/easy/bit/IntegerReplacement.java
@@ -0,0 +1,14 @@
+package src.java.easy.bit;
+
+public class IntegerReplacement {
+    public int integerReplacement(int n) {
+        int c = 0;
+        while (n != 1) {
+            if ((n & 1) == 0) n >>>= 1;
+            else if (n==3 || ((n>>>1) & 1) == 0)   n--;
+            else n++;
+            c++;
+        }
+        return c;
+    }
+}
