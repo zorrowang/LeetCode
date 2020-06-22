d60d8eb9ccd7b8c7c797d429e58e5c0b0624d358
Hongwwei Wang <zorrowanghw@gmail.com>
2020-06-21 22:08:15 -0400

    Update 385 Mini Parser

--- src/java/medium/string/MiniParser.java
+++ src/java/medium/string/MiniParser.java
@@ -1,32 +1,32 @@
+package src.java.medium.string;
+
 /**
- * // This is the interface that allows for creating nested lists.
- * // You should not implement it, or speculate about its implementation
- * public interface NestedInteger {
- *     // Constructor initializes an empty nested list.
- *     public NestedInteger();
+ * // This is the interface that allows for creating nested lists. // You should
+ * not implement it, or speculate about its implementation public interface
+ * NestedInteger { // Constructor initializes an empty nested list. public
+ * NestedInteger();
  *
- *     // Constructor initializes a single integer.
- *     public NestedInteger(int value);
+ * // Constructor initializes a single integer. public NestedInteger(int value);
  *
- *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
- *     public boolean isInteger();
+ * // @return true if this NestedInteger holds a single integer, rather than a
+ * nested list. public boolean isInteger();
  *
- *     // @return the single integer that this NestedInteger holds, if it holds a single integer
- *     // Return null if this NestedInteger holds a nested list
- *     public Integer getInteger();
+ * // @return the single integer that this NestedInteger holds, if it holds a
+ * single integer // Return null if this NestedInteger holds a nested list
+ * public Integer getInteger();
  *
- *     // Set this NestedInteger to hold a single integer.
- *     public void setInteger(int value);
+ * // Set this NestedInteger to hold a single integer. public void
+ * setInteger(int value);
  *
- *     // Set this NestedInteger to hold a nested list and adds a nested integer to it.
- *     public void add(NestedInteger ni);
+ * // Set this NestedInteger to hold a nested list and adds a nested integer to
+ * it. public void add(NestedInteger ni);
  *
- *     // @return the nested list that this NestedInteger holds, if it holds a nested list
- *     // Return null if this NestedInteger holds a single integer
- *     public List<NestedInteger> getList();
- * }
+ * // @return the nested list that this NestedInteger holds, if it holds a
+ * nested list // Return null if this NestedInteger holds a single integer
+ * public List<NestedInteger> getList(); }
  */
-public class Solution {
+
+public class MiniParser {
     public NestedInteger deserialize(String s) {
         NestedInteger ret = new NestedInteger();
         if (s.startsWith("[")) {
