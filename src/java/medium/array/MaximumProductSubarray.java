836bcd74f2dbfa9865b6817c7215612c28aa419b
Hongwwei Wang <zorrowanghw@gmail.com>
2021-01-22 04:35:23 -0500

    Update MaximumProductSubarray

--- src/java/medium/array/MaximumProductSubarray.java
+++ src/java/medium/array/MaximumProductSubarray.java
@@ -1,7 +1,21 @@
 package src.java.medium.array;
 
 public class MaximumProductSubarray {
+    // Optimize space usage
     public int maxProduct(int[] nums) {
+        int ret = nums[0];
+        int minEnding = nums[0], maxEnding = nums[0];
+        for (int i=1; i<nums.length; i++) {
+            int min = Math.min(minEnding*nums[i], maxEnding*nums[i]);
+            int max = Math.max(minEnding*nums[i], maxEnding*nums[i]);
+            minEnding = Math.min(nums[i], min);
+            maxEnding = Math.max(nums[i], max);
+            ret = Math.max(ret, maxEnding);
+        }
+        return ret;
+    }
+
+    public int maxProduct2(int[] nums) {
         if (nums == null || nums.length == 0)   return 0;
         int ret = nums[0];
         int[] maxProductEndHere = new int[nums.length];
