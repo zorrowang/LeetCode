b0351107838e197e6221d1257bae7357ff594019
Hongwwei Wang <zorrowanghw@gmail.com>
2021-01-23 08:37:02 -0500

    Update LongestIncreasingPathMatrix

--- src/java/hard/backtracking/LongestIncreasingPathMatrix.java
+++ src/java/hard/backtracking/LongestIncreasingPathMatrix.java
@@ -31,4 +31,38 @@ public class LongestIncreasingPathMatrix {
         cache[startM][startN] = max;
         return max;
     }
+
+    // Rewritten on 2021/01/21
+    // A bit faster than the previous version
+    public int longestIncreasingPath2(int[][] matrix) {
+        int ret = 0;
+        int m = matrix.length, n = matrix[0].length;
+        int[][] cache = new int[m][n];
+        for (int i=0; i<m; i++)
+            for (int j=0; j<n; j++)
+                ret = Math.max(ret, dfs2(matrix, i, j, cache));
+        return ret;
+    }
+    
+    private int dfs2(int[][] matrix, int i, int j, int[][] cache) {
+        if (cache[i][j] > 0)    return cache[i][j];
+        if ((i==0 || matrix[i][j]>matrix[i-1][j]) && 
+            (i==matrix.length-1 || matrix[i][j]>matrix[i+1][j]) && 
+            (j==0 || matrix[i][j]>matrix[i][j-1]) && 
+            (j==matrix[i].length-1 || matrix[i][j]>matrix[i][j+1])) {
+            cache[i][j] = 1;
+            return 1;
+        }
+        int up=0, down=0, left=0, right=0;
+        if (i>0 && matrix[i][j]<matrix[i-1][j])
+            up = dfs2(matrix, i-1, j, cache);
+        if (i<matrix.length-1 && matrix[i][j]<matrix[i+1][j])
+            down = dfs2(matrix, i+1, j, cache);
+        if (j>0 && matrix[i][j]<matrix[i][j-1])
+            left = dfs2(matrix, i, j-1, cache);
+        if (j<matrix[i].length-1 && matrix[i][j]<matrix[i][j+1])
+            right = dfs2(matrix, i, j+1, cache);
+        cache[i][j] = Math.max(up, Math.max(down, Math.max(left, right)))+1;
+        return cache[i][j];
+    }
 }
