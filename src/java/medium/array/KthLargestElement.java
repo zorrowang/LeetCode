1fa1d98786e363ec343d3dfbc0e4b37763c5de5f
Hongwwei Wang <zorrowanghw@gmail.com>
2021-01-23 05:59:12 -0500

    Update findKthLargest

--- src/java/medium/array/KthLargestElement.java
+++ src/java/medium/array/KthLargestElement.java
@@ -15,4 +15,29 @@ public class KthLargestElement {
         }
         return pq.peek();
     }
+
+    // QuickSelect solution
+    public int findKthLargest2(int[] nums, int k) {
+        return quickSelect(nums, 0, nums.length-1, k);
+    }
+    
+    private int quickSelect(int[] nums, int lo, int hi, int k) {
+        int pivot = lo;
+        for (int i=lo; i<hi; i++) {
+            if (nums[i] <= nums[hi])
+                swap(nums, pivot++, i);
+        }
+        swap(nums, pivot, hi);
+        
+        int cnt = hi - pivot + 1;
+        if (cnt < k)    return quickSelect(nums, lo, pivot-1, k-cnt);
+        else if (cnt > k)   return quickSelect(nums, pivot+1, hi, k);
+        else return nums[pivot];
+    }
+    
+    private void swap(int[]nums, int i, int j) {
+        int t = nums[i];
+        nums[i] = nums[j];
+        nums[j] = t;
+    }
 }
