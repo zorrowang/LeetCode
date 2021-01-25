4c2406733027774bd36178414eccb2dfa4945e4e
Hongwwei Wang <zorrowanghw@gmail.com>
2021-01-25 00:25:12 -0500

    Update ValidAnagram

--- src/java/easy/string/ValidAnagram.java
+++ src/java/easy/string/ValidAnagram.java
@@ -1,7 +1,21 @@
 package src.java.easy.string;
 
 public class ValidAnagram {
+    //Rewritten on 2021/01/25
     public boolean isAnagram(String s, String t) {
+        if (s==null || t==null) return false;
+        if (s.length() != t.length())   return false;
+        int[] cnts = new int[256];
+        for (int i=0; i<s.length(); i++) {
+            cnts[s.charAt(i)]++;
+            cnts[t.charAt(i)]--;
+        }
+        for (int i=0; i<cnts.length; i++)
+            if (cnts[i]!=0) return false;
+        return true;
+    }
+    
+    public boolean isAnagram2(String s, String t) {
         int[] counts = new int[26];
         for (int i=0; i<s.length(); i++)
             counts[s.charAt(i)-'a']++;
